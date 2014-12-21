7645332c7d709708e919566a14a13bcfb14331ad
Andrey Galkin <andrey@futoin.eu>
2014-12-21 18:30:26 +0200

    Updated unit tests

--- test/unittest.js
+++ test/unittest.js
@@ -14,6 +14,10 @@ var impl = {
     {
         reqinfo.result().n = reqinfo.params().n;
         as.success( { b : reqinfo.params().a } );
+    },
+    
+    noResult : function( as, reqinfo )
+    {
     }
 };
 
@@ -98,6 +102,14 @@ describe( 'Executor', function(){
     });
     
     describe('#process', function(){
+        beforeEach(function(){
+            as = async_steps();
+            executor = new executor_module.Executor( ccm, opts );
+            as.add( function( as ){
+                executor.register( as, 'fileface.derived:2.3', impl );
+            } );
+        });
+        
         it('should process request', function( done ){
             reqinfo = new executor_module.RequestInfo( executor, {
                 f : "fileface.derived:2.3:normal_call",
@@ -106,9 +118,7 @@ describe( 'Executor', function(){
                 }
             } );
             
-            as.add( function( as ){
-                executor.register( as, 'fileface.derived:2.3', impl );
-            }).add(
+            as.add(
                 function( as ){
                     as.state.reqinfo = reqinfo;
                     executor.process( as );
@@ -131,5 +141,123 @@ describe( 'Executor', function(){
             });
             as.execute();
         });
+        
+        it('should process with error', function( done ){
+            reqinfo = new executor_module.RequestInfo( executor, {
+                f : "fileface.derived:2.3:normal_call",
+                p : {}
+            } );
+            
+            as.add(
+                function( as ){
+                    as.state.reqinfo = reqinfo;
+                    executor.process( as );
+                },
+                function( as, err ){
+                    done( new Error( err + ": " + as.state.error_info ) );
+                }
+            ).add( function( as ){
+                try
+                {
+                    var rsp = as.state.reqinfo.info()[ reqinfo.INFO_RAW_RESPONSE ];
+                    rsp.should.equal( '{"e":"InvalidRequest"}' );
+                    done();
+                }
+                catch ( e )
+                {
+                    done( e );
+                }
+            });
+            as.execute();
+        });
+        
+        it('should correctly handle no result calls', function( done ){
+            as.add(
+                function( as ){
+                    reqinfo = new executor_module.RequestInfo( executor, {
+                        f : "fileface.derived:2.3:noResult",
+                        p : {}
+                    } );
+
+                    as.state.reqinfo = reqinfo;
+                    executor.process( as );
+                },
+                function( as, err ){
+                    done( new Error( err + ": " + as.state.error_info ) );
+                }
+            ).add( function( as ){
+                try
+                {
+                    assert.strictEqual( null, as.state.reqinfo.info()[ reqinfo.INFO_RAW_RESPONSE ] );
+                    done();
+                }
+                catch ( e )
+                {
+                    done( e );
+                }
+            });
+            as.execute();
+        });
+        
+        it('should correctly handle forcersp calls', function( done ){
+            as.add(
+                function( as ){
+                    reqinfo = new executor_module.RequestInfo( executor, {
+                        f : "fileface.derived:2.3:noResult",
+                        p : {},
+                        forcersp : true
+                    } );
+
+                    as.state.reqinfo = reqinfo;
+                    executor.process( as );
+                },
+                function( as, err ){
+                    done( new Error( err + ": " + as.state.error_info ) );
+                }
+            ).add( function( as ){
+                try
+                {
+                    as.state.reqinfo.info()[ reqinfo.INFO_RAW_RESPONSE ].should.equal('{"r":{}}');
+                    done();
+                }
+                catch ( e )
+                {
+                    done( e );
+                }
+            });
+            as.execute();
+        });
     });
 });
+
+describe( 'UserInfo', function(){
+    it('should basically function', function(){
+        var userinfo = new executor_module.UserInfo( null, 'LID123', 'GID123' );
+        userinfo.localID().should.equal( 'LID123' );
+        userinfo.globalID().should.equal( 'GID123' );
+    });
+});
+
+describe( 'SourceAddress', function(){
+    it('should basically function with IPv4', function(){
+        var saddr = new executor_module.SourceAddress( null, '1.2.3.4', '345' );
+        saddr.asString().should.be.equal( 'IPv4:1.2.3.4:345' );
+    });
+    it('should basically function with IPv6', function(){
+        var saddr = new executor_module.SourceAddress( null, '2001:db8::ff00:42:8329', '345' );
+        saddr.asString().should.be.equal( 'IPv6:[2001:db8::ff00:42:8329]:345' );
+    });
+    it('should basically function with LOCAL', function(){
+        var saddr = new executor_module.SourceAddress( null, null, '/path/to.socket' );
+        saddr.asString().should.be.equal( 'LOCAL:/path/to.socket' );
+    });
+});
+
+describe( 'DerivedKey', function(){
+    it('should basically function', function(){
+        var userinfo = new executor_module.DerivedKey( null, 1234, 5678 );
+        userinfo.baseID().should.equal( 1234 );
+        userinfo.sequenceID().should.equal( 5678 );
+    });
+});
+
