522da989b49f06ca28c5bf30c0f35729fd8c2591
Andrey Galkin <andrey@futoin.eu>
2015-02-12 23:58:52 +0000

    closes #7: Executor: implement stress testing
    - Implemented bare simple Stress-Performance-Interoperation test
    - NOTE: there is a failure in AsyncSteps loop under heavy stress after about 30seconds of run (TODO)

--- test/spi/client.js
+++ test/spi/client.js
@@ -1,11 +1,169 @@
 'use strict';
 
+var async_steps = require( 'futoin-asyncsteps' );
 var invoker_module = require( 'futoin-invoker' );
+var child_process = require( 'child_process' );
+var util = require('util');
 
 var opts = {
     prodMode : true,
     callTimeoutMS : 2e3,
-    specDirs : __dirname + '../specs'
+    specDirs : __dirname + '/../specs',
+    messageSniffer_disabled : function( info, rawmsg, isreq )
+    {
+        console.dir( rawmsg );
+    },
 };
 
-var ccm = new invoker_module.AdvancedCCM( opts );
+
+var argv = require('minimist')(
+        process.argv.slice(2),
+        {
+            default : {
+                time_limit : 60e3,
+                concurrency : 500,
+            }            
+        }
+);
+//---
+var made_calls = 0;
+var success_calls = 0;
+var failed_calls = 0;
+
+function print_stats()
+{
+    var mem = process.memoryUsage();
+    console.log( "S:"+success_calls+" F:"+failed_calls+" T:"+made_calls+" MEMUSED:"+mem.heapUsed+"/"+mem.heapTotal+"@"+mem.rss );
+}
+
+//---
+
+var model_as = async_steps();
+model_as.add(
+    function( as )
+    {
+        var ccm = new invoker_module.AdvancedCCM( opts );
+        
+        as.add(
+            function( as )
+            {
+                ccm.register( as, 'test', 'spi.test:0.1', 'http://127.0.0.1:34567/stress/' );
+            },
+            function( as, err )
+            {
+                console.log( err );
+            }
+        );
+        
+        as.loop( function( as ){
+            var iface = ccm.iface( 'test' );
+            as.parallel().add(
+                function( as ){
+                    iface.call( as, 'normalCall', { a : 'a' } );
+                    ++made_calls;
+                    
+                    as.add( function( as, rsp )
+                    {
+                        if ( rsp.b != 'a' ) as.error( 'MyError' );
+                        ++success_calls;
+                    } );
+                },
+                function( as, err ){
+                    ++failed_calls;
+                    //console.log( as.state.last_exception.stack );
+                    as.success();
+                }
+            ).add(
+                function( as ){
+                    iface.call( as, 'noResult', { a : 'a' } );
+                    ++made_calls;
+                    
+                    as.add( function( as )
+                    {
+                        ++success_calls;
+                    } );
+                },
+                function( as, err ){
+                    ++failed_calls;
+                    //console.log( as.state.last_exception.stack );
+                    as.success();
+                }
+            ).add(
+                function( as ){
+                    iface.call( as, 'errorCall' );
+                    ++made_calls;
+                    
+                    as.add( function( as )
+                    {
+                        ++failed_calls;
+                    } );
+                },
+                function( as, err ){
+                    if ( err === 'MyError' )
+                    {
+                        ++success_calls;
+                    }
+                    else
+                    {
+                        ++failed_calls;
+                    }
+                    
+                    as.success();
+                }
+            );
+        } );
+    },
+    function( as, err )
+    {
+        console.log( err, as.state.error_info );
+        console.log( as.state.last_exception.stack );
+    }
+);
+
+//---
+var aslist = new Array( argv.concurrency );
+
+for ( var as, c = aslist.length - 1; c >= 0; --c )
+{
+    as = async_steps();
+    as.copyFrom( model_as );
+    aslist[ c ] = as;
+}
+
+
+//---
+console.log( 'FORKING' );
+var child = child_process.fork( __dirname + '/server.js' );
+child.send( { test: true } );
+
+child.on( 'message', function(){
+    var interval = setInterval( function(){
+        print_stats();
+    }, 1e3 );
+
+    setTimeout( function(){
+        for ( c = aslist.length - 1; c >= 0; --c )
+        {
+            aslist[ c ].cancel();
+        }
+        
+        console.log( '-------' );
+        console.log( 'END' );
+        console.log( '-------' );
+        print_stats();
+        clearInterval( interval );
+        child.kill();
+    }, argv.time_limit );
+
+    print_stats();
+    console.log( '-------' );
+    console.log( 'START' );
+    console.log( '-------' );
+
+    for ( c = aslist.length - 1; c >= 0; --c )
+    {
+        aslist[ c ].execute();
+    }
+} );
+
+//---
--- test/spi/server.js
+++ test/spi/server.js
@@ -1,16 +1,70 @@
-l'use strict';
+'use strict';
 
+var async_steps = require( 'futoin-asyncsteps' );
 var invoker_module = require( 'futoin-invoker' );
 var executor_module = require( '../../lib/main' );
+var util = require('util');
 
 var opts = {
     prodMode : true,
-    specDirs : __dirname + '../specs',
-    httpAddr : '0.0.0.0',
+    specDirs : __dirname + '/../specs',
+    httpAddr : '127.0.0.1',
     httpPort : 34567,
     httpPath : '/stress/',
     httpBacklog : 4096,
+    messageSniffer_disabled : function( info, rawmsg, isreq )
+    {
+        console.dir( rawmsg );
+    }
 };
+
+function print_stats()
+{
+    var mem = process.memoryUsage();
+    console.log( "SERVER MEMUSED:"+mem.heapUsed+"/"+mem.heapTotal+"@"+mem.rss );
+}
 
 var ccm = new invoker_module.AdvancedCCM( opts );
 var executor = new executor_module.NodeExecutor( ccm, opts );
+var impl = {};
+
+executor.on('ready', function(){
+    async_steps().add(
+        function( as )
+        {
+            executor.register( as, 'spi.test:0.1', impl );
+        },
+        function( as, err )
+        {
+            //console.log( err + " " + as.state.error_info );
+            console.log( as.state.last_exception.stack );
+        }
+    )
+    .add( function( as ) {
+        process.send({ ready : 'ok' });
+    })
+    .execute();
+    
+    print_stats();
+    
+    setInterval( print_stats, 1e3 );
+});
+
+
+impl.normalCall = function( as, reqinfo )
+{
+    reqinfo.result().b = reqinfo.params().a;
+};
+
+impl.noResult = function( as, reqinfo )
+{};
+
+impl.errorCall = function( as, reqinfo )
+{
+    as.error( 'MyError' );
+};
+
+impl.rawUpload = function( as, reqinfo )
+{
+    reqinfo.result().b = reqinfo.params().a;
+};
